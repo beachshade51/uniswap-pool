4b050a504d46d730beaf7fa3596212bdf89ab5ff
Sidharth P <sidharthp@phaethon.tech>
2023-04-05 21:17:43 +0530

    typo fixed

--- contracts/sidCoinB.sol
+++ contracts/sidCoinB.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract sidCoinA is ERC20 {
+contract sidCoinB is ERC20 {
     constructor() ERC20("sidCoinB", "sidCoinB") {
         _mint(msg.sender, 100000 * 10 ** 18);
     }
